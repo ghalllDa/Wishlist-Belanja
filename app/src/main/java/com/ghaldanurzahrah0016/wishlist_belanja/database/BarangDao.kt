b68abadbc9bf5cb9bbe1b224dc0c562aa52ee977
Ghalda Nur Zahrah <nurzahraghalda@gmail.com>
2026-05-15 19:37:54 +0700

    membuat db dan dao

--- app/src/main/java/com/ghaldanurzahrah0016/wishlist_belanja/database/BarangDao.kt
+++ app/src/main/java/com/ghaldanurzahrah0016/wishlist_belanja/database/BarangDao.kt
@@ -1,4 +1,27 @@
 package com.ghaldanurzahrah0016.wishlist_belanja.database
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.ghaldanurzahrah0016.wishlist_belanja.model.Barang
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface BarangDao {
+
+    @Insert
+    suspend fun insert(barang: Barang)
+
+    @Update
+    suspend fun upadate(barang: Barang)
+
+    @Query("SELECT * FROM barang ORDER BY nama DESC")
+    fun getBarang(): Flow<List<Barang>>
+
+    @Query("SELECT * FROM barang WHERE id = :id")
+    suspend fun getBarangById(id : Long): Barang?
+
+    @Query("DELETE FROM barang WHERE id = :id")
+    suspend fun deleteById(id: Long)
 }
--- app/src/main/java/com/ghaldanurzahrah0016/wishlist_belanja/database/BarangDb.kt
+++ app/src/main/java/com/ghaldanurzahrah0016/wishlist_belanja/database/BarangDb.kt
@@ -1,4 +1,35 @@
 package com.ghaldanurzahrah0016.wishlist_belanja.database
 
-class BarangDb {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.ghaldanurzahrah0016.wishlist_belanja.model.Barang
+
+@Database(entities = [Barang::class], version = 1, exportSchema = false)
+abstract class BarangDb : RoomDatabase() {
+
+    abstract val dao: BarangDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: BarangDb? = null
+
+        fun getInstance(context: Context): BarangDb {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        BarangDb::class.java,
+                        "=barang.db"
+                    ).build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 }
