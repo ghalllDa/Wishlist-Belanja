17cd3bedc33ad0efd32a2eccdd2c47b6f7bc5bb9
Ghalda Nur Zahrah <nurzahraghalda@gmail.com>
2026-05-15 20:56:54 +0700

    membuat DetailScreen

--- app/src/main/java/com/ghaldanurzahrah0016/wishlist_belanja/DisplayAlertDialog.kt
+++ app/src/main/java/com/ghaldanurzahrah0016/wishlist_belanja/DisplayAlertDialog.kt
@@ -1,2 +1,43 @@
 package com.ghaldanurzahrah0016.wishlist_belanja
 
+import android.content.res.Configuration
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.ghaldanurzahrah0016.wishlist_belanja.ui.theme.Wishlist_BelanjaTheme
+
+@Composable
+fun DisplayAlertDialog(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit
+){
+    AlertDialog(
+        text = { Text(text = stringResource(R.string.pesan_hapus)) },
+        confirmButton = {
+            TextButton(onClick = { onConfirmation() }) {
+                Text(text = stringResource(R.string.tombol_hapus))
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = { onDismissRequest() }) {
+                Text(text = stringResource(R.string.tombol_batal))
+            }
+        },
+        onDismissRequest = { onDismissRequest() }
+    )
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun DialogPreview() {
+    Wishlist_BelanjaTheme() {
+        DisplayAlertDialog(
+            onDismissRequest = {},
+            onConfirmation = {}
+        )
+    }
+}
--- app/src/main/java/com/ghaldanurzahrah0016/wishlist_belanja/ui/screen/DetailScreen.kt
+++ app/src/main/java/com/ghaldanurzahrah0016/wishlist_belanja/ui/screen/DetailScreen.kt
@@ -1,2 +1,204 @@
 package com.ghaldanurzahrah0016.wishlist_belanja.ui.screen
 
+import android.content.res.Configuration
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.ghaldanurzahrah0016.wishlist_belanja.DisplayAlertDialog
+import com.ghaldanurzahrah0016.wishlist_belanja.R
+import com.ghaldanurzahrah0016.wishlist_belanja.ui.theme.Wishlist_BelanjaTheme
+import com.ghaldanurzahrah0016.wishlist_belanja.util.ViewModelFactory
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen(navController: NavHostController, id: Long? = null) {
+    val context = LocalContext.current
+    val factory = ViewModelFactory(context)
+    val viewModel: DetailViewModel = viewModel(factory = factory)
+
+    var nama by remember { mutableStateOf("") }
+    var jumlah by remember { mutableStateOf("") }
+    var harga by remember { mutableStateOf("") }
+    var showDialog by remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit) {
+        if (id == null) return@LaunchedEffect
+        val data = viewModel.getBarang(id) ?: return@LaunchedEffect
+        nama = data.nama
+        jumlah = data.jumlah
+        harga = data.harga
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack()  }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.kembali),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                },
+                title = {
+                    if (id == null)
+                        Text(text = stringResource(R.string.tambah_catatan))
+                    else
+                        Text(text = stringResource(R.string.edit_barang))
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                ),
+                actions = {
+                    IconButton(onClick = {
+                        if (nama == "" || jumlah == "" || harga == ""){
+                            Toast.makeText(context, R.string.invalid, Toast.LENGTH_SHORT).show()
+                            return@IconButton
+                        }
+                        if (id == null) {
+                            viewModel.insert(nama, jumlah, harga)
+                        } else {
+                            viewModel.update(nama, jumlah, harga)
+                        }
+                        navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.Outlined.Check,
+                            contentDescription = stringResource(R.string.simpan),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                    if (id != null) {
+                        DeleteAction {
+                            showDialog = true
+                        }
+                    }
+                }
+            )
+        }
+    ) { padding ->
+        FormCatatan(
+            title =nama,
+            onTitleChange = { nama = it },
+            desc = jumlah,
+            onDescChange = { jumlah = it },
+            modifier = Modifier.padding(padding),
+        )
+
+        if (id != null && showDialog) {
+            DisplayAlertDialog(
+                onDismissRequest = { showDialog = false }) {
+                showDialog = false
+                viewModel.delete(id)
+                navController.popBackStack()
+            }
+        }
+    }
+}
+
+@Composable
+fun DeleteAction(delete: () -> Unit) {
+    var expanded by remember { mutableStateOf(false) }
+
+    IconButton(onClick = {expanded = true}) {
+        Icon(
+            imageVector = Icons.Filled.MoreVert,
+            contentDescription = stringResource(R.string.lainnya),
+            tint = MaterialTheme.colorScheme.primary
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                text = {
+                    Text(text = stringResource(R.string.hapus))
+                },
+                onClick = {
+                    expanded = false
+                    delete()
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun FormCatatan(
+    title: String, onTitleChange: (String) -> Unit,
+    desc: String, onDescChange: (String) -> Unit,
+    modifier: Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        OutlinedTextField(
+            value = title,
+            onValueChange = { onTitleChange(it) },
+            label = { Text(text = stringResource(R.string.nama)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Words,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = desc,
+            onValueChange = { onDescChange(it) },
+            label = { Text(text = stringResource(R.string.jumlah)) },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Sentences
+            ),
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun DetailScreenPreview() {
+    Wishlist_BelanjaTheme{
+        DetailScreen(rememberNavController())
+    }
+}
